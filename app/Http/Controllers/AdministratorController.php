3c667edd6de68f147c578623fbdf56af15cbd927
bailram <>
2021-03-18 18:40:55 +0700

    update typo in AdministratorController

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Storage;
 
 class AdministratorController extends Controller
 {
